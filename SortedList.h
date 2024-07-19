5fd83a0af02ac548e1a1c65bc7c3cfe3ce805072
yussefz99 <yussefz@campus.technion.ac.il>
2024-07-19 19:19:15 +0300

    working on sortedlist.h

--- SortedList.h
+++ SortedList.h
@@ -5,6 +5,35 @@
 
 namespace mtm {
 
+    /// class Node --------------------------------------------------------
+
+    template<typename T>
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T& val) : data(val), next(nullptr) {}
+    };
+//    class Node{
+//    private:
+//        T* data;
+//        Node<T>* next;
+//    public:
+//        explicit Node(const T& data):data(data),next(nullptr){}
+//        Node():data(nullptr),next(nullptr){}
+//        ~Node<T>(){
+//            delete data;
+//        }
+//        T* get_data(){
+//            return data;
+//        }
+//        Node<T>* getNext(){
+//            return next;
+//        }
+//    };
+
+
+/// class Sortedlist --------------------------------------------------------
+
     template <typename T>
     class SortedList {
     public:
@@ -31,27 +60,173 @@ namespace mtm {
          * 11. filter - returns a new list with elements that satisfy a given condition
          * 12. apply - returns a new list with elements that were modified by an operation
          */
+         ///constructors and destructor:
+         SortedList():head(nullptr),size(0){}
+
+         SortedList(const SortedList<T>& other):head(nullptr),size(other.size){  // add exspation?
+             Node<T>* ptr=other.head;
+             Node<T>* nextInorder = nullptr;
+             while (ptr != nullptr){
+                 Node<T>* newNode=new Node<T>(ptr->data);
+                 if(head == nullptr){
+                     head=newNode;
+                 } else{
+                     nextInorder->next=newNode;
+                 }
+                 nextInorder = newNode;
+                 ptr=ptr->next;
+             }
+         }
+
+         SortedList& operator=(const SortedList<T>& other){
+             if(this == &other)return *this;
+             Node<T>* ptr=other.head;
+             Node<T>* nextInorder = nullptr;
+             while (ptr != nullptr){
+                 Node<T>* newNode=new Node<T>(ptr->data);
+                 if(head == nullptr){
+                     head=newNode;
+                 } else{
+                     nextInorder->next=newNode;
+                 }
+                 nextInorder = newNode;
+                 ptr=ptr->next;
+             }
+             size=other.size;
+             return *this;
+         }
+         ~SortedList(){
+             while (head) {
+                 Node<T>* temp = head;
+                 head = head->next;
+                 delete temp;
+             }
+         }
+         //-----------------------------------------------------
+         ///iterator:
+         class ConstIterator;
+        ConstIterator begin()const{
+            return ConstIterator(this,0);
+        }
+        ConstIterator end()const{
+            return ConstIterator(this,size);
+        }
+         //-----------------------------------------------------
+         ///functions:
+         void insert(T data){ // T* data ??
+             Node<T>* newNode=new Node<T>(data);
+             if (!head || data > head->data) {
+                 newNode->next = head;
+                 head = newNode;
+             } else {
+                 Node<T>* curr = head;
+                 while (curr->next && curr->next->data > data) {//????????????????>=
+                     curr = curr->next;
+                 }
+                 newNode->next = curr->next;
+                 curr->next = newNode;
+             }
+             size++;
+         }
+
+         void remove(const ConstIterator& it){
+             if (it.index < 0 || it.index >= size) {
+                 throw std::out_of_range("out_of_range");
+             }
+
+             if (it.index == 0) {
+                 Node<T>* temp = head;
+                 head = head->next;
+                 delete temp;
+             } else {
+                 Node<T>* nextInorder = head;
+                 int count = 0;
+                 while (count < it.index - 1) {
+                     nextInorder = nextInorder->next;
+                     count++;
+                 }
+                 Node<T>* temp = nextInorder->next;
+                 nextInorder->next = temp->next;
+                 delete temp;
+             }
+             size--;
+         }
+
+         int length()const{
+             return this->size;
+        }
+
+
+//---------------------------------------------------------
+         friend bool operator==(const SortedList<T>& l1 ,const SortedList<T>& l2){ //?????????
+             if(l1.size != l2.size)return false;
+             Node<T>* ptr1=l1.head;
+             Node<T>* ptr2=l2.head;
+             while (ptr1 && ptr2){
+                 if(ptr1->get_data() != ptr2->get_data())return false;
+                 ptr1=ptr1->next;
+                 ptr2=ptr2->next;
+             }
+             return true;
+         }
+         //-----------------------------------------------------------------
+
+    private:
+        Node<T>* head;
+        int size;
 
     };
+
+
+    /// class ConstIterator --------------------------------------------------------
 
     template <class T>
     class SortedList<T>::ConstIterator {
-    /**
-     * the class should support the following public interface:
-     * if needed, use =defualt / =delete
-     *
-     * constructors and destructor:
-     * 1. a ctor(or ctors) your implementation needs
-     * 2. copy constructor
-     * 3. operator= - assignment operator
-     * 4. ~ConstIterator() - destructor
-     *
-     * operators:
-     * 5. operator* - returns the element the iterator points to
-     * 6. operator++ - advances the iterator to the next element
-     * 7. operator!= - returns true if the iterator points to a different element
-     *
-     */
-    };
-}
+    private:
+        int index;
+        const SortedList* list;
+        friend class SortedList;
+        ConstIterator(const SortedList* list,int index):list(list),index(index){}
+    public:
+        ConstIterator(const ConstIterator& other)=default;
+        ConstIterator& operator=(const ConstIterator& other)=default;
+        ~ConstIterator()=default;
+        ConstIterator& operator++(){
+            if(index == list->size){
+                throw std::out_of_range("out_of_range");
+            }
+            index++;
+            return *this;
+        }
 
+        ConstIterator operator++(int){
+            ConstIterator iter=*this;
+            ++*this;
+            return iter;
+        }
+
+        bool operator==(const ConstIterator& other)const{
+            if(list==other.list && this->index == other.index)return true; // list =list ???
+            return false;
+        }
+
+        bool operator!=(const ConstIterator& other)const{
+            return !(*this==other);
+        }
+
+        const T& operator*()const{
+            if(index < 0 || index >= list->size){
+                throw std::out_of_range("out_of_range");
+            }
+            Node<T>* ptr=list->head;
+            int count=0;
+            while (count <= index-1){
+                ptr=ptr->next;
+                count++;
+            }
+            return ptr->data;
+        }
+
+    };
+
+}
